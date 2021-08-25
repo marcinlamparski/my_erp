2ab9551ddcec3a5d06e63f4a8665e2220818ed03
marcinlamparski <m.lamparski@gmail.com>
2021-08-25 11:41:02 +0200

    rozbudowano modele

--- kadry/admin.py
+++ kadry/admin.py
@@ -1,6 +1,88 @@
-from django.contrib import admin
+from django.contrib import admin 
 
 # Register your models here.
-from .models import Employee
+from .models import Pracownik
+from .models import Klient
+from .models import Projekt
+from .models import Zlecenie
+from .models import Moja_Firma
+from .models import Marszruta
+from .models import Operacje_technologiczne
+from imagekit.admin import AdminThumbnail #pakiet do tworzenia miniatur
 
-admin.site.register(Employee)
+
+admin.site.site_header = 'Panel Kadry - Kaczmarczyk'  #ustawia nagłówek zmiast "Django Administracja"
+admin.site.site_title = 'Administracja MES Kaczmarczyk'
+
+@admin.register(Moja_Firma)
+class Moja_FirmaAdmin(admin.ModelAdmin):
+	list_display = ('nazwa',)
+
+
+@admin.register(Pracownik)
+class PracownikAdmin(admin.ModelAdmin):
+	list_display = ('aktywny', 'nazwisko', 'imie', 'nr_telefonu','rola', 'foto_display')
+	list_filter = ('aktywny', 'bhp_wygasa', 'badania_wygasaja', 'rola', 'narodowosc')
+	search_fields = ('nazwisko',)
+	ordering = ('nazwisko',)
+	foto_display = AdminThumbnail(image_field='foto_miniatura') #wyswietlanie miniatur w list_display
+	foto_display.short_description = 'Fotografia'
+	readonly_fields = ['foto_display']
+
+@admin.register(Klient)
+class KlientAdmin(admin.ModelAdmin):
+	list_display = ('nazwa',)
+	search_fields = ('nazwa',)
+	ordering = ('nazwa',)
+	readonly_fields = ['data_dodania']
+
+
+@admin.register(Projekt)
+class ProjektAdmin(admin.ModelAdmin):
+	list_display = ('id', 'nazwa', 'get_customer_name', 'get_project_owner',)
+	list_filter = ('klient', 'szef_projektu',)
+	search_fields = ('nazwa',)
+	ordering = ('nazwa',)
+	readonly_fields = ['data_dodania']
+
+	def get_customer_name(self, obj):  #metoda do pobrania nazwy klienta
+		return obj.klient.nazwa
+	get_customer_name.short_description = 'Klient'
+	get_customer_name.admin_order_field = 'nazwa__klienta'
+
+	def get_project_owner(self, obj):  #metoda do pobrania nazwiska właściciela projektu
+		return obj.szef_projektu.nazwisko + obj.szef_projektu.imie
+	get_project_owner.short_description = 'Właściciel'
+	get_project_owner.admin_order_field = 'nazwisko__właściciela__projektu'
+
+
+@admin.register(Zlecenie)
+class ZlecenieAdmin(admin.ModelAdmin):
+	list_display = ('id','nazwa', 'get_project_name')
+
+	def get_project_name(self, obj):  #metoda do pobrania nazwy projektu
+		return obj.nr_projektu.nazwa
+	get_project_name.short_description = 'Projekt'
+	get_project_name.admin_order_field = 'nazwa__projektu'
+
+
+@admin.register(Marszruta)
+class MarszrutaAdmin(admin.ModelAdmin):
+	fields = ('nazwa', 'id',)             #ustawia kolejność w trybie edycji
+	list_display = ('id', 'nazwa', 'get_order_name', 'get_operacja',)
+
+	def get_order_name(self, obj):  #metoda do pobrania nr zlecenia
+		return obj.zlecenie.id
+	get_order_name.short_description = 'Zlecenie'
+	get_order_name.admin_order_field = 'numer__zlecenia'
+
+	def get_operacja(self, obj):                         # metoda pobiera liste operacji po symbolu
+		return "\n".join([p.symbol for p in obj.operacja.all()])
+	get_operacja.short_description = 'Operacje'
+	get_operacja.admin_order_field = 'Operacje technologiczne'
+	
+
+@admin.register(Operacje_technologiczne)
+class Operacje_technologiczneAdmin(admin.ModelAdmin):
+	list_display = ('nazwa', 'symbol', 'czas_przezbrajania')
+	
--- kadry/models.py
+++ kadry/models.py
@@ -5,29 +5,143 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-class Employee(models.Model):
-	first_name = models.CharField(max_length=50)
-	last_name = models.CharField(max_length=50)
+from PIL import Image #pakiet do obslugi zdjec
+from imagekit.models import ImageSpecField #pakiet do obslugi zdjec
+from imagekit.processors import ResizeToFill #pakiet do obslugi zdjec
+
+from django import forms #pola formularzy html
+
+
+
+#class Employee(models.Model):
+	#first_name = models.CharField(max_length=50)
+	#last_name = models.CharField(max_length=50)
+	#pesel = models.CharField(max_length=11, null=True)
+	#ROLE = (
+	#	('PRO', 'Projektant'),
+	#	('KP', 'Kierownik Produkcji'),
+	#	('PP', 'Pracownik Produkcji'),
+	#	('MON', 'Montażysta'),
+	#	('KZ', 'Kadra Zarządzająca'),
+	#)
+	#data_dodania = models.DateTimeField(default=timezone.now)
+	#birth_date = models.DateField(auto_now=False, null=True, blank=True)
+	#salary_netto = models.DecimalField(max_digits=5, decimal_places=2,null=True)
+	#ohs_expires_date = models.DateField(auto_now=False, null=True, blank=True)
+	#health_exam_expires_date = models.DateField(auto_now=False, null=True, blank=True)
+	#viza_expires_date = models.DateField(auto_now=False, null=True, blank=True)
+	#nationality = (
+	#	('PL', 'Polskie'),
+	#	('UA', 'Ukraiśkie'),
+	#	)
+	#passport_no = models.CharField(max_length=9, null=True)
+	#id_card_no = models.CharField(max_length=9, null=True)
+	#phone_no = models.CharField(max_length=9, null=True)
+	#email = models.EmailField(max_length=50, null=True)
+	#health_exam_file = models.FileField(null=True)
+
+class Moja_Firma(models.Model):
+	nazwa = models.CharField(max_length=50)
+	nip = models.CharField(max_length=12, help_text="NIP w formacie PLXXXXXXXXXX")
+	data_dodania = models.DateTimeField(auto_now_add=True)
+
+
+	class Meta:
+		verbose_name_plural = "Moja Firma"
+
+
+class Pracownik(models.Model):
+	nazwisko = models.CharField(max_length=50)
+	imie = models.CharField(max_length=50)
 	pesel = models.CharField(max_length=11, null=True)
-	ROLE = (
+	AKTYWNY_CHOICES  = (
+		(True, 'Aktywny'),
+		(False, 'Niektywny')
+		)
+	aktywny = models.BooleanField(default=False)
+	ROLA_CHOICES = (
 		('PRO', 'Projektant'),
 		('KP', 'Kierownik Produkcji'),
 		('PP', 'Pracownik Produkcji'),
 		('MON', 'Montażysta'),
-		('KZ', 'Kadra Zarządzająca'),
+		('KPR', 'Kierownik Projektu'),
+		('DYR', 'Dyrektor'),
 	)
+	rola = models.CharField(max_length=3, choices=ROLA_CHOICES)
 	data_dodania = models.DateTimeField(default=timezone.now)
-	birth_date = models.DateField(auto_now=False, null=True, blank=True)
-	salary_netto = models.DecimalField(max_digits=5, decimal_places=2,null=True)
-	ohs_expires_date = models.DateField(auto_now=False, null=True, blank=True)
-	health_exam_expires_date = models.DateField(auto_now=False, null=True, blank=True)
-	viza_expires_date = models.DateField(auto_now=False, null=True, blank=True)
-	nationality = (
+	data_zatrudnienia = models.DateField(auto_now=False, null=True, blank=True)
+	data_zwolnienia = models.DateField(auto_now=False, null=True, blank=True)
+	data_urodzenia = models.DateField(auto_now=False, null=True, blank=True)
+	wynagrodzenie_netto = models.DecimalField(max_digits=5, decimal_places=2,null=True)
+	bhp_wygasa = models.DateField(auto_now=False, null=True, blank=True)
+	badania_wygasaja = models.DateField(auto_now=False, null=True, blank=True)
+	viza_wygasa = models.DateField(auto_now=False, null=True, blank=True)
+	NARODOWOSC_CHOICES = (
 		('PL', 'Polskie'),
 		('UA', 'Ukraiśkie'),
 		)
-	passport_no = models.CharField(max_length=9, null=True)
-	id_card_no = models.CharField(max_length=9, null=True)
-	phone_no = models.CharField(max_length=9, null=True)
-	email = models.EmailField(max_length=50, null=True)
-	health_exam_file = models.FileField(null=True)
+	narodowosc = models.CharField(max_length=2, choices=NARODOWOSC_CHOICES, verbose_name = 'narodowość')
+	nr_paszportu = models.CharField(max_length=9, null=True, blank=True, help_text = 'W przypadku obcokrajowców', verbose_name = 'numer paszportu')
+	nr_dowodu= models.CharField(max_length=9, null=True, verbose_name = 'numer dowodu')
+	nr_telefonu = models.CharField(max_length=9, null=True, verbose_name = 'numer telefonu')
+	email = models.EmailField(max_length=50, null=True, blank=True)
+	notatki = models.TextField(null=True, blank=True)
+	foto = models.ImageField(upload_to='foto', default='/foto/default_foto.jpeg')
+	foto_miniatura = ImageSpecField(source='foto',
+                                      processors=[ResizeToFill(30, 30)],
+                                      format='JPEG',
+                                      options={'quality': 50})
+
+	class Meta:
+		verbose_name_plural = "Pracownicy"
+
+
+class Klient(models.Model):
+	nazwa = models.CharField(max_length=50, unique=True)
+	notatki = models.TextField(null=True, blank=True, help_text = 'Istotne informacje dotyczące klienta')
+	data_dodania = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		verbose_name_plural = "Klienci"
+
+
+class Projekt(models.Model):
+	nazwa = models.CharField(max_length=50)
+	notatki = models.TextField(null=True, blank=True, help_text = 'Ważne informacje dotyczące projeku')
+	klient = models.ForeignKey(Klient, on_delete=models.CASCADE, null=True)
+	szef_projektu = models.ForeignKey(Pracownik, on_delete=models.CASCADE, null=True, verbose_name = 'szef projektu')
+	data_dodania = models.DateTimeField(auto_now_add=True)
+	ostatnia_modyfikacja = models.DateField(auto_now=True)
+	
+	class Meta:
+		verbose_name_plural = "Projekty"
+
+class Zlecenie(models.Model):
+	#numer = models.CharField(max_length=50)
+	nazwa = models.CharField(max_length=50)
+	data_dodania = models.DateTimeField(auto_now_add=True)
+	ostatnia_modyfikacja = models.DateField(auto_now=True)
+	nr_projektu = models.ForeignKey(Projekt, on_delete=models.CASCADE, null=True)
+
+	class Meta:
+		verbose_name_plural = "Zlecenia"
+
+
+
+class Operacje_technologiczne(models.Model):
+	nazwa = models.CharField(max_length=50)
+	symbol = models.CharField(max_length=3, help_text = 'Maksymalnie 3 znaki')
+	czas_przezbrajania = models.PositiveSmallIntegerField(default=0, verbose_name='Czas przezbrajania maszyny', help_text = 'Jednostka - godzina')
+
+	class Meta:
+		verbose_name_plural = "Operacje"
+
+
+
+class Marszruta(models.Model):
+	nazwa = models.CharField(max_length=50)
+	zlecenie = models.ForeignKey(Zlecenie, on_delete=models.CASCADE, null=True)
+	operacja = models.ManyToManyField(Operacje_technologiczne)
+	class Meta:
+		verbose_name_plural = "Marszruty"
+	
